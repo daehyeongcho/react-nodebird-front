07e5871c161d86c458cf3d225f0ecfb8e30d4638
PC-102ILSEONG <dhcho1034@gmail.com>
2021-08-23 18:02:40 +0900

    inline 스타일링으로 회귀

--- components/FollowList.js
+++ components/FollowList.js
@@ -1,44 +1,30 @@
-import React, { useMemo, useCallback } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { List, Button, Card } from 'antd'
 import { StopOutlined } from '@ant-design/icons'
 
 const FollowList = ({ header, data }) => {
-	const action = useMemo(() => [<StopOutlined key='stop' />], [])
-	const style = useMemo(
-		() => ({
-			listStyle: { marginBottom: 20 },
-			listGrid: { gutter: 4, xs: 2, md: 3 },
-			loadMoreStyle: { textAlign: 'center', margin: '10px 0' },
-			listItemStyle: { marginTop: 20 },
-		}),
-		[],
-	)
-	const renderListItem = useCallback(
-		(item) => (
-			<List.Item style={style.listItemStyle}>
-				<Card actions={action}>
-					<Card.Meta description={item.nickname} />
-				</Card>
-			</List.Item>
-		),
-		[],
-	)
 	return (
 		<>
 			<List
-				style={style.listStyle}
-				grid={style.listGrid}
+				style={{ marginBottom: 20 }}
+				grid={{ gutter: 4, xs: 2, md: 3 }}
 				size='small'
 				header={<div>{header}</div>}
 				loadMore={
-					<div style={style.loadMoreStyle}>
+					<div style={{ textAlign: 'center', margin: '10px 0' }}>
 						<Button>더 보기</Button>
 					</div>
 				}
 				bordered
 				dataSource={data}
-				renderItem={renderListItem}
+				renderItem={(item) => (
+					<List.Item style={{ marginTop: 20 }}>
+						<Card actions={[<StopOutlined key='stop' />]}>
+							<Card.Meta description={item.nickname} />
+						</Card>
+					</List.Item>
+				)}
 			></List>
 		</>
 	)
