5fd2f9ac671a816274f2267698924283def30c0b
PC-102ILSEONG <dhcho1034@gmail.com>
2021-08-23 17:53:53 +0900

    이미지 Carousel 구현

--- components/PostImages.js
+++ components/PostImages.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PropTypes from 'prop-types'
+import { PlusOutlined } from '@ant-design/icons'
+
+import ImagesZoom from './ImagesZoom'
 
 const PostImages = ({ images }) => {
-	console.log(images)
+	const [showImagesZoom, setShowImagesZoom] = useState(false)
+	const onZoom = useCallback(() => {
+		setShowImagesZoom(true)
+	}, [])
+	const onClose = useCallback(() => {
+		setShowImagesZoom(false)
+	})
+
+	if (images.length === 1) {
+		return (
+			<>
+				<img role='presentation' src={images[0].src} alt={images[0].src} onClick={onZoom} />
+				{showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+			</>
+		)
+	}
+	if (images.length === 2) {
+		return (
+			<>
+				<img role='presentation' style={{ display: 'inline-block', width: '50%' }} src={images[0].src} alt={images[0].src} onClick={onZoom} />
+				<img role='presentation' style={{ display: 'inline-block', width: '50%' }} src={images[1].src} alt={images[1].src} onClick={onZoom} />
+				{showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+			</>
+		)
+	}
 	return (
-		<div>
-			<br />
-		</div>
+		// 3개 이상일 땐 이미지 하나만 보여주고 나머지는 더보기 버튼
+		<>
+			<div>
+				<img role='presentation' width='50%' src={images[0].src} alt={images[0].src} onClick={onZoom} />
+				<div
+					role='presentation'
+					style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+					onClick={onZoom}
+				>
+					<PlusOutlined />
+					<br />
+					{images.length - 1}개의 사진 더보기
+				</div>
+			</div>
+			{showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+		</>
 	)
 }
 
