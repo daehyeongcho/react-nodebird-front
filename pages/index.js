13904e9a9a5173d42667a67ed74cda902fa03307
PC-102ILSEONG <dhcho1034@gmail.com>
2021-09-03 18:02:04 +0900

    서버사이드렌더링 준비

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { END } from 'redux-saga'
+
+import wrapper from '../store/configureStore'
+import { loadMyInfoRequest } from '../actions/user'
+import { loadPostsRequest } from '../actions/post'
 
 import AppLayout from '../components/AppLayout/AppLayout' // 공통 메뉴 및 레이아웃
 import PostForm from '../components/PostForm/PostForm' // 새 글 쓰기 폼
 import PostCard from '../components/PostCard/PostCard' // 기존의 글들을 렌더링하는 컴포넌트
-import { loadMyInfoRequest } from '../actions/user'
-import { loadPostsRequest } from '../actions/post'
 
 /** 메인 페이지
  * - 로그인 완료 시 글쓰기 창을 띄우고, 밑에 기존 글들을 불러옴.
@@ -23,11 +26,6 @@ const Home = () => {
 			alert(retweetError) // eslint-disable-line no-alert
 		}
 	}, [retweetError])
-
-	useEffect(() => {
-		dispatch(loadMyInfoRequest()) // 로그인 된 사용자 정보 불러옴
-		dispatch(loadPostsRequest()) // 모든 글들 불러옴
-	}, [])
 
 	/* 스크롤 */
 	useEffect(() => {
@@ -60,5 +58,15 @@ const Home = () => {
 		</>
 	)
 }
+
+/* 여기에 넣어두면 Home보다 먼저 실행됨(SSR) */
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => {
+	console.log(store)
+	store.dispatch(loadMyInfoRequest()) // 로그인 된 유저 정보 불러옴
+	store.dispatch(loadPostsRequest()) // 트윗 목록 불러옴
+
+	store.dispatch(END) // REQUEST -> SUCCESS 될 때까지 기다려줌.
+	await store.sagaTask.toPromise()
+})
 
 export default Home
--- reducers/index.js
+++ reducers/index.js
@@ -4,18 +4,14 @@ import { combineReducers } from 'redux'
 import user from './user'
 import post from './post'
 
-const rootReducer = combineReducers({
-	index: (state = {}, action) => {
-		switch (action.type) {
-			case HYDRATE: // SSR 때문에 추가
-				console.log('HYDRATE', action)
-				return { ...state, ...action.payload }
-			default:
-				return state
-		}
-	},
-	user,
-	post,
-})
+const rootReducer = (state, action) => {
+	switch (action.type) {
+		case HYDRATE:
+			console.log('HYDRATE', action)
+			return action.payload
+		default:
+			return combineReducers({ user, post })(state, action) // 기존 reducer에 case 추가로 집어넣기 위해 이런식으로 확장을 함
+	}
+}
 
 export default rootReducer
